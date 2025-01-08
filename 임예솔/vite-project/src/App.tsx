ec230e54c0f1f90d6eaa688844c0e51872a05754
sonamu <skit21sol@gmal.com>
2025-01-08 18:53:27 +0900

    기초

---
+++ 임예솔/vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const ClimbingMap: = () => {
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div>
+      <h1>충청북도 클라이밍장 지도</h1>
+    
+    </div>
+  );
+};
+
+export default ClimbingMap;
